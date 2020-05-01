8e12c5e17d2acd02ef914d2dcde36f049d5c3dca
Matheus Guimaraes <matheusguimr@gmail.com>
2020-05-01 17:08:01 -0300

    Add yelpcamp final version

---
+++ app.js
@@ -0,0 +1,56 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var passport = require("passport");
+var flash = require('connect-flash');
+var LocalStrategy = require("passport-local");
+var methodOverride = require('method-override');
+var Campground = require('./models/campground');
+var Comment = require('./models/comment');
+var User = require('./models/user');
+var seedDB = require("./seeds");
+var port = 3001;
+
+var indexRoutes = require("./routes/index");
+var campgroundRoutes = require("./routes/campgrounds");
+var commentRoutes = require("./routes/comments");
+
+app.use(require("express-session")({
+    secret: "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+mongoose.connect("mongodb://localhost/yelp_camp", {
+    useNewUrlParser: true
+});
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.set("view engine", "ejs");
+app.use(flash());
+
+app.use(function (req, res, next) {
+    res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
+    next();
+})
+
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments/", commentRoutes);
+
+app.listen(port, function () {
+    console.log("The YelpCamp Server Has Started!");
+});
---
+++ seeds.js
@@ -0,0 +1,54 @@
+var mongoose = require('mongoose');
+var Campground = require('./models/campground');
+var Comment = require('./models/comment');
+
+var data = [{
+    name: "Cloud's Rest",
+    image: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    description: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like"
+}, {
+    name: "Desert Mesa",
+    image: "https://images.unsplash.com/photo-1537905569824-f89f14cceb68?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    description: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like"
+}, {
+    name: "Canyon Floor",
+    image: "https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    description: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like"
+}];
+
+function seedDB() {
+    // Remove all campgrounds
+    Campground.deleteMany({}, function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("removed campgrounds!");
+            // add a few campgrounds
+            data.forEach(function (seed) {
+                Campground.create(seed, function (err, campground) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log("added a campground!");
+                        // create a comment
+                        Comment.create({
+                                text: "This place is great, but I whish there was internet",
+                                author: "Homer"
+                            },
+                            function (err, comment) {
+                                if (err) {
+                                    console.log(err);
+                                } else {
+                                    campground.comments.push(comment);
+                                    campground.save();
+                                    console.log("created new comment");
+                                }
+                            });
+                    }
+                });
+            });
+        }
+    });
+};
+
+module.exports = seedDB;

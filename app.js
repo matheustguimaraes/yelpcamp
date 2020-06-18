0806964fb593b79178160522923761acd067f909
Matheus Guimaraes <matheusguimr@gmail.com>
2020-06-18 15:52:25 -0300

    Comment MongoDB connection

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
+// var mongoose = require("mongoose");
 var passport = require("passport");
 var flash = require('connect-flash');
 var LocalStrategy = require("passport-local");
 var methodOverride = require('method-override');
-var Campground = require('./models/campground');
-var Comment = require('./models/comment');
 var User = require('./models/user');
 var seedDB = require("./seeds");
 var port = 3001;
@@ -28,9 +26,9 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-mongoose.connect("mongodb://localhost/yelp_camp", {
-    useNewUrlParser: true
-});
+// mongoose.connect("mongodb://localhost/yelp_camp", {
+//     useNewUrlParser: true
+// });
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -52,5 +50,5 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments/", commentRoutes);
 
 app.listen(port, function () {
-    console.log("The YelpCamp Server Has Started!");
+    console.log("YelpCamp has started in http://localhost:3001");
 });

d4d9c7baea570c2ab6386a926bae1105432efdda
Matheus Guimaraes <matheusguimr@gmail.com>
2020-06-18 15:51:57 -0300

    Add mongodb data in constant

---
+++ db.js
@@ -0,0 +1,164 @@
+const data = [
+  {
+    name: "Ohio Lake",
+    price: 30,
+    image:
+      "https://images.unsplash.com/photo-1471115853179-bb1d604434e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    description: "Dino Reichmuth",
+    author: {
+      id: {
+        type: 1,
+        ref: "user1",
+      },
+      username: "a",
+    },
+    comments: [
+      {
+        type: 1,
+        ref: "comment1",
+      },
+    ],
+  },
+  {
+    name: "Night refuge",
+    price: 30,
+    image:
+      "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    description: "Scott Goodwill",
+    author: {
+      id: {
+        type: 1,
+        ref: "user1",
+      },
+      username: "a",
+    },
+    comments: [
+      {
+        type: 1,
+        ref: "comment1",
+      },
+    ],
+  },
+  {
+    name: "Silver river",
+    price: 30,
+    image:
+      "https://images.unsplash.com/photo-1492648272180-61e45a8d98a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    description: "Glen Jackson",
+    author: {
+      id: {
+        type: 1,
+        ref: "user1",
+      },
+      username: "a",
+    },
+    comments: [
+      {
+        type: 1,
+        ref: "comment1",
+      },
+    ],
+  },
+  {
+    name: "Front firepit",
+    price: 30,
+    image:
+      "https://images.unsplash.com/photo-1530541930197-ff16ac917b0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    description: "Toa Heftiba",
+    author: {
+      id: {
+        type: 1,
+        ref: "user1",
+      },
+      username: "a",
+    },
+    comments: [
+      {
+        type: 1,
+        ref: "comment1",
+      },
+    ],
+  },
+  {
+    name: "Drake's Desert",
+    price: 30,
+    image:
+      "https://images.unsplash.com/photo-1523987355523-c7b5b0dd90a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    description: "Blake Wisz",
+    author: {
+      id: {
+        type: 1,
+        ref: "user1",
+      },
+      username: "a",
+    },
+    comments: [
+      {
+        type: 1,
+        ref: "comment1",
+      },
+    ],
+  },
+  {
+    name: "North's Festival",
+    price: 30,
+    image:
+      "https://images.unsplash.com/photo-1520095972714-909e91b038e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    description: "Krists Luhaers",
+    author: {
+      id: {
+        type: 1,
+        ref: "user1",
+      },
+      username: "a",
+    },
+    comments: [
+      {
+        type: 1,
+        ref: "comment1",
+      },
+    ],
+  },
+  {
+    name: "Alaska Cold camp",
+    price: 30,
+    image:
+      "https://images.unsplash.com/photo-1455496231601-e6195da1f841?ixlib=rb-1.2.1&auto=format&fit=crop&w=1395&q=80",
+    description: "David Schultz",
+    author: {
+      id: {
+        type: 1,
+        ref: "user1",
+      },
+      username: "a",
+    },
+    comments: [
+      {
+        type: 1,
+        ref: "comment1",
+      },
+    ],
+  },
+  {
+    name: "Bright night",
+    price: 30,
+    image:
+      "https://images.unsplash.com/photo-1494545261862-dadfb7e1c13d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    description: "Adam Eggleston",
+    author: {
+      id: {
+        type: 1,
+        ref: "user1",
+      },
+      username: "a",
+    },
+    comments: [
+      {
+        type: 1,
+        ref: "comment1",
+      },
+    ],
+  },
+];
+
+module.exports = data;
